e72ac3bd8bf4a6ff6667fa97599e08d93ec1b475
z4zenn <145710449+z4zenn@users.noreply.github.com>
2026-02-27 14:39:55 +0500

    Update oneMoretask.js

--- someMoretasks.js
+++ someMoretasks.js
@@ -1,25 +1,14 @@
 // Task 22
-let timeLeft=10;
-console.log(" time is ticking...");
-const timerId=setInterval(()=>{
-    console.log(timeLeft);
-    timeLeft--;
-    if(timeLeft<0)
+let timeLeft = 10;
+console.log("time is ticking...");
+
+const timerId = setInterval(() => {
+  console.log(timeLeft);
+  timeLeft--;
+
+  if (timeLeft < 0) {
     clearInterval(timerId);
-    console.log("Time's Up my nigga");
-},1000);
+    console.log("Time's up!");
+  }
 
-
-
-// let timeLeft = 10;
-// console.log("time is ticking...");
-
-// const timerId = setInterval(() => {
-//   console.log(timeLeft);
-//   timeLeft--;
-
-//   if (timeLeft < 0) {
-//     clearInterval(timerId);
-//     console.log("Time's up!");
-//   }
-// }, 1000);
+}, 1000);
